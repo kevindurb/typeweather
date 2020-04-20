1f588c9724dcbdd0f1fee7f67f70543f33e3bd7c
Kevin Durbin <kevindurb@gmail.com>
2020-04-20 15:48:42 -0600

    add time

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as datefns from 'date-fns';
 import * as weather from './hooks/weather';
 import * as location from './hooks/location';
 import HourlyTemperature from './components/HourlyTemperature';
@@ -8,6 +9,7 @@ import CurrentWeather from './components/CurrentWeather';
 function App() {
   const [locationData, fetchLocation] = location.useCurrentLocation();
   const [weatherData, fetchWeather] = weather.useWeatherData(locationData?.lat ?? 0, locationData?.lon ?? 0);
+  const [now, setNow] = React.useState(new Date());
 
   React.useEffect(() => {
     if (!locationData) {
@@ -17,12 +19,26 @@ function App() {
     }
   }, [fetchLocation, fetchWeather, locationData, weatherData]);
 
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date())
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  const nowString = React.useMemo(() => datefns.format(now, 'PPpp'), [now])
+
   if (!weatherData) return null;
 
   return (
     <div className="container">
-      <div className="row justify-content-center my-3">
-        <h2 className="text-secondary text-center col">{locationData?.city}, {locationData?.region}</h2>
+      <div className="row justify-content-center">
+        <h6 className="text-secondary text-center col">{locationData?.city}, {locationData?.region}</h6>
+      </div>
+      <div className="row justify-content-center">
+        <h6 className="text-secondary text-center col">{nowString}</h6>
       </div>
       <CurrentWeather
         currentData={weatherData.current}
