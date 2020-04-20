84b311ee0a60af8b6dbdd492d459ff264260f5d3
Kevin Durbin <kevindurb@gmail.com>
2020-04-20 16:40:37 -0600

    fix ssl

--- src/hooks/weather.ts
+++ src/hooks/weather.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as api from './api';
 import { IconType } from '../components/Icon';
 
-const BASE_URL = 'http://api.openweathermap.org/data/2.5';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 const TOKEN = process.env.REACT_APP_OPEN_WEATHER_TOKEN as string;
 
 type WeatherIcon = '04d';
