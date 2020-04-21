50b49d6210c67dca7722404563a058bc0e853483
Kevin Durbin <kevindurb@gmail.com>
2020-04-21 11:35:45 -0600

    get geocode data

--- src/hooks/location.ts
+++ src/hooks/location.ts
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import * as qs from 'querystring';
+import * as api from './api';
 
 interface LocationResponse {
   latitude: number;
   longitude: number;
+  locality?: string;
+  principalSubdivision?: string;
+}
+
+interface GeocodeResponse {
+  locality: string;
+  principalSubdivision: string;
 }
 
 export const useCurrentLocation = (): [
@@ -21,13 +29,43 @@ export const useCurrentLocation = (): [
   );
 
   const fetchLocation = React.useCallback(() => {
-    navigator.geolocation.getCurrentPosition((result) => {
-      setLocation({
-        latitude: result.coords.latitude,
-        longitude: result.coords.longitude,
-      });
-    });
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
   }, []);
 
-  return [location, fetchLocation];
+  const geocodeParams = React.useMemo(() => ({
+    latitude: `${location?.latitude}`,
+    longitude: `${location?.longitude}`
+  }), [location]);
+
+  const [geoData, fetchGeoData] = api.useGet<GeocodeResponse>('https://api.bigdatacloud.net/data/reverse-geocode-client', geocodeParams)
+
+  React.useEffect(() => {
+    if (!geoData && location) {
+      fetchGeoData();
+    }
+  }, [geoData, fetchGeoData, location])
+
+  React.useEffect(() => {
+    if (geoData && location) {
+      setLocation({
+        latitude: location.latitude,
+        longitude: location.longitude,
+        locality: geoData.locality,
+        principalSubdivision: geoData.principalSubdivision,
+      })
+    }
+  }, [geoData, setLocation, location])
+
+  const fetchAll = React.useCallback(async () => {
+    try {
+      const result = await fetchLocation();
+      console.log(result);
+    } catch (e) {
+      console.log(e)
+    }
+  }, [fetchLocation]);
+
+  return [location, fetchAll];
 };
